ca59970e14d8bc821fb1eb478c8667297fc67682
marinoscar <oscar@marin.cr>
2020-07-08 09:12:45 -0500

    added more configuration options

--- code/luval.recorder/RecordingInfo.cs
+++ code/luval.recorder/RecordingInfo.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace luval.recorder
 {
+    /// <summary>
+    /// Information about the recording attributes
+    /// </summary>
     public class RecordingInfo
     {
         public RecordingInfo()
@@ -14,10 +17,36 @@ namespace luval.recorder
             FileName = string.Format("{0}\\ScreenRecording-{1}.mp4", Environment.CurrentDirectory,  SessionName);
             MaxDurationInMinutes = 3;
             IntervalTimeInMs = 250;
+            MaxRecordingMinutes = 120;
+            ConsoleScreenStartup = Program.SW_MINIMIZE;
         }
+        /// <summary>
+        /// Full file path for the recording
+        /// </summary>
         public string FileName { get; set; }
+        /// <summary>
+        /// Max number of minutes for the screen recording
+        /// </summary>
         public int MaxDurationInMinutes { get; set; }
+        /// <summary>
+        /// Number of milliseconds between each screen capture 
+        /// </summary>
         public int IntervalTimeInMs { get; set; }
+        /// <summary>
+        /// Name of the recording session
+        /// </summary>
         public string SessionName { get; set; } 
+        /// <summary>
+        /// Indicates if the session would open a named pipe to get messages about the recording
+        /// </summary>
+        public bool UseNamedPipes { get; set; }
+        /// <summary>
+        /// The recording session would be active to a max of minutes
+        /// </summary>
+        public int MaxRecordingMinutes { get; set; }
+        /// <summary>
+        /// Indicates the screen position, 0 = Hide, 5 = Show or 6 minimize
+        /// </summary>
+        public int ConsoleScreenStartup { get; set; }
     }
 }
