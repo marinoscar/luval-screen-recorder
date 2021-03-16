c0a5119954004f2841bfb9fe12b776c63624448a
Oscar Marin <oscar@marin.cr>
2021-03-15 20:33:54 -0500

    added a new class to enable to have the file store more data and not just limit to a single line of text

---
+++ code/luval.recorder.fileshare/ProcessShareData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace luval.recorder.fileshare
+{
+    public class ProcessShareData : Dictionary<string, string>
+    {
+        public override string ToString()
+        {
+            return Serialize();
+        }
+
+        private string Serialize()
+        {
+            var lines = new List<string>();
+            foreach (var item in this)
+            {
+                lines.Add(string.Format("{0};{1}", item.Key.Replace(";", "&^!"), item.Value.Replace(";","&^!")));
+            }
+            return string.Join(Environment.NewLine, lines);
+        }
+    }
+}
